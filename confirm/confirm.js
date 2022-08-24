9a11f0c045d7cc39c0783b8a6df20caff4f6497f
alpherg0221 <alpherg0221@gmail.com>
2022-08-24 19:40:28 +0900

    8/24 Fix html

--- confirm/confirm.js
+++ confirm/confirm.js
@@ -1,9 +1,17 @@
 // #移行の文字列を取得
 let dest = window.location.hash.substring(1);
 
-// aタグ (id=dest) を取得
+// h3タグ (id=dest) を取得
 let destElem = document.getElementById("dest");
-
-// aタグのhrefと表示する文字を書き換え
-destElem.setAttribute("href", dest);
+// h3タグの表示する文字を書き換え
 destElem.innerText = dest;
+
+// OKボタン
+let okButton = document.getElementById("ok");
+okButton.onclick = () => window.location.href = dest;
+
+// Cancelボタン
+let cancelButton = document.getElementById("cancel");
+cancelButton.onclick = () => {
+    window.open("about:blank", "_self").close();
+};
