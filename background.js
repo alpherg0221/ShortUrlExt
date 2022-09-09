9fe85c7d5dece85e655ef7e92898144e7ff85898
alpherg0221 <alpherg0221@gmail.com>
2022-09-09 16:19:05 +0900

    9/9 Changed redirect blocking mechanism

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 chrome.webRequest.onBeforeRequest.addListener(
     async function (details) {
+        /*
         // ブロックルールを無効化
         await chrome.declarativeNetRequest.updateEnabledRulesets({
             disableRulesetIds: ["ruleset_1"]
         });
+        */
     },
     {
         'urls': ['<all_urls>'],
@@ -15,19 +17,20 @@ chrome.webRequest.onHeadersReceived.addListener(
     async function (details) {
         // 300番台かチェック
         if (Math.floor(details.statusCode / 100) === 3) {
-            // Todo:ブロックルールに正規表現を使って短縮URLに絞る
-            // ブロックルールを有効化
-            await chrome.declarativeNetRequest.updateEnabledRulesets({
-                enableRulesetIds: ["ruleset_1"]
-            });
-
-            // 遷移先URLを取得
-            let [dest] = details.responseHeaders.filter(header => header.name === "location");
-
             // 現在のタブ (リダイレクトがブロックされた) を取得
             let [tabBlocked] = await chrome.tabs.query({active: true, currentWindow: true});
-            // 現在のタブ (リダイレクトがブロックされた) を削除
-            await chrome.tabs.remove(tabBlocked.id);
+
+            // about:blankならタブを閉じる
+            // それ以外なら読み込みを停止
+            if (tabBlocked.url === "") {
+                await chrome.tabs.remove(tabBlocked.id);
+            } else {
+                await chrome.tabs.discard(tabBlocked.id);
+            }
+
+            // Todo:絶対URLで取得する (現在は相対URLの場合もある)
+            // 遷移先URLを取得
+            let [dest] = details.responseHeaders.filter(header => header.name === "location");
 
             // 確認ページを開く
             await chrome.tabs.create({url: `./confirm/confirm.html#${dest.value}`});
