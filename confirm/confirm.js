6e19b5ad642bfcaf1854a7a3b7d8d8643ead0d3b
alpherg0221 <alpherg0221@gmail.com>
2022-09-09 17:53:58 +0900

    9/9 Add thumbnail support

--- confirm/confirm.js
+++ confirm/confirm.js
@@ -15,3 +15,12 @@ let cancelButton = document.getElementById("cancel");
 cancelButton.onclick = () => {
     window.open("about:blank", "_self").close();
 };
+
+// Thumbnailのimg
+let thumbnailImg = document.getElementById("thumbnail_img");
+
+// Thumbnailボタン
+let thumbnailButton = document.getElementById("thumbnail");
+thumbnailButton.onclick = async () => {
+    thumbnailImg.src = `https://capture.heartrails.com/400x400/cool/shorten?${dest}`;
+};
