aee8092529a7355a03e80b7ec85ce3df1c02f581
saItama320 <70998025+saItama320@users.noreply.github.com>
2022-09-29 00:25:35 +0900

    Update thumb.py

--- backend/logic/thumb.py
+++ backend/logic/thumb.py
@@ -1,27 +1,27 @@
+from email.mime import base
 from fastapi import APIRouter
-import requests
-import cv2
+from fastapi import FastAPI
 import os
-import sys
+import cv2
 import base64
-import hashlib
-import json
 
 router = APIRouter()
 
-@router.get("/trace")
+@router.get("/thumbnail")
 
-def trace_handler(url): #shortURLがくる
-    # ファイル名を用意する
-    thumbnail = f"{hash(url)}.png" #ハッシュ関数を作る
-    # ブラウザ制御コマンドを実行する
-    output = exec.run("ikezawa_command {url} {thumbnail}")
-    # コマンドの出力をjsonの形式にする(outputの形式が分かり次第いろいろ変更)
-    keys = ["from_url", "term_url", "chains", "thums", "info"]
-    values = []
-    output_dict = json.dump(dict(zip(keys, values)))
-    return output_dict
+def thumbnail_handler(token: str, size: int=400):  # 引数はtoken(画像の識別子)と画像サイズ
+  thumb_file = f"{token}_{size}.png"
+  
+  if not os.path.isfile(thumb_file): # 画像がなければ生成する
+    decode_token = base64.b64decode(token).decode()
+    original = decode_token + ".png"
+    img = cv2.imread(original)
+    write_img_file(img, thumb_file, size)
+    #cv2.imwrite(thumb_file, img.resize(size))
+  # ファイルを送る
+  app = FastAPI()
+  app.post(thumb_file)
 
-def hash(url):
-    #return hashlib.sha256(url.encode()).hexdigest()
-    return base64.b64encode(url.encode())
+def write_img_file(img, thumb_file, size):
+  resize_img = cv2.resize(img, dsize=(size,size))
+  cv2.imwrite(thumb_file, resize_img) 
