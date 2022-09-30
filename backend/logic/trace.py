150f394cc61a161a032acb2668b403cc8e72d9fa
kita <nanbu.kenkyu@gmail.com>
2022-09-30 18:58:26 +0900

    0930 add modify trace

--- backend/logic/trace.py
+++ backend/logic/trace.py
@@ -8,35 +8,32 @@ import hashlib
 import json
 sys.path.append("./ipc/")
 import dispatcher
+import task
+import filestore
 
 router = APIRouter()
 
 @router.get("/trace")
 
 def trace_handler(url): #shortURLがくる
-    d = dispatcher.Dispatcher()
     # ファイル名を用意する
     thumbnail = f"{hash(url)}" #ハッシュ関数を作る
     # ブラウザ制御コマンドを実行する
-    disp = dispatcher.Dispatcher()
-    disp.register({"url": url, "thumbnail": thumbnail})
-    completed = disp.complete()
+    t = task.Task()
+    completed = t.run({"url": url, "thumbnail": thumbnail})
     result = json.loads(completed["result"])
     if "thumbnail" in result:
-        disp.download_thumbnail(result["thumbnail"])
+        filestore.pull(result["thumbnail"])
     inf = json.loads(completed["result"])
-    disp.close()
 
     # コマンドの出力をjsonの形式にする(outputの形式が分かり次第いろいろ変更)
     keys = ["from_url", "term_url", "chains", "thums"]
     values = [inf["src"], inf["dst"], inf["chain"], inf["thumbnail"]]
     output_dict = dict(zip(keys, values))
-    print(output_dict)
     return output_dict
 
 def hash(url):
     return hashlib.sha256(url.encode()).hexdigest()
-    #return base64.b64encode(url.encode())
 
 if __name__ == "__main__":
     trace_handler("http://google.com")
