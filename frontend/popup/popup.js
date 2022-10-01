177f04b939b782fadd2ab3ba59b837795184d601
alpherg0221 <alpherg0221@gmail.com>
2022-10-02 02:39:01 +0900

    10/2 Change confirm page

--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -4,3 +4,9 @@ whitelistEditButton.onclick = async () => {
     // whitelistページに移動
     await chrome.tabs.create({url: '../whitelist/editor/whitelistEditor.html'});
 };
+
+// helpボタン
+let helpButton = document.getElementById("help");
+helpButton.onclick = async () => {
+    await chrome.tabs.create({url: '../help/help.html'});
+};
