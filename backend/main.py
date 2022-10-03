0881a7112f1ad6d7f2f0787c05b8fa9c69ebbee2
pfpfdev <pfpfdev@gmail.com>
2022-10-03 15:39:11 +0900

    10/03 split phase of tasks

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from logic import thumb, trace, ws
+from logic import thumb, trace, ws, detail
 
 app = FastAPI()
 
 app.include_router(thumb.router)
 app.include_router(trace.router)
 app.include_router(ws.router)
+app.include_router(detail.router)
