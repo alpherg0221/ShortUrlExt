9a5d10442fc3aafcdf597e23f8e7d13d3457d37a
alpherg0221 <alpherg0221@gmail.com>
2022-10-07 01:25:31 +0900

    10/7 Added the function to check if the server is running

--- frontend/popup/server/server.js
+++ frontend/popup/server/server.js
@@ -1,5 +1,7 @@
+// デフォルトのサーバ名
 const defaultServerName = "mws2022.pfpfdev.net";
 
+// 右上の戻るボタン
 const backButton = document.getElementById("back_button");
 backButton.onclick = () => {
     location.href = "../popup.html";
@@ -10,20 +12,37 @@ const icon = document.getElementById("icon");
 // checkテキスト
 const checkText = document.getElementById("check_label");
 
+// 保存されているサーバ名を取得
+const serverName = await getServerName();
+
+// テキストフィールド要素を取得
 const serverInput = document.getElementById("server_input");
-serverInput.value = await getServerName();
+
+// テキストフィールドの中身を保存されているサーバ名に設定
+serverInput.value = serverName;
+
+// MDCのテキストフィールドのインスタンスを生成
 const serverIn = new mdc.textField.MDCTextField(document.querySelector(".mdc-text-field"));
 
+// デフォルトに戻すボタン
 const defaultButton = document.getElementById("default");
 defaultButton.onclick = async () => {
     serverInput.value = defaultServerName;
 };
 
+// 保存ボタン
 const saveButton = document.getElementById("save");
 saveButton.onclick = async () => {
-    const serverName = serverInput.value;
-    await setServerName(serverName);
-    location.href = "../popup.html";
+    try {
+        await fetch(`https://${serverInput.value}/docs/`);
+        await setServerName(serverInput.value);
+        location.href = "../popup.html";
+    } catch (e) {
+        icon.innerText = "warning";
+        icon.style.color = "#EA4335";
+        checkText.innerText = "サーバに到達できません";
+        checkText.style.color = "red";
+    }
 };
 
 async function getServerName() {
